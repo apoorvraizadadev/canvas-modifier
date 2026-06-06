1938716f6f5bb901b18d4be72f612e1c30b1da66
apoorvraizadadev <apoorv.raizada.dev@gmail.com>
2026-06-06 01:11:06 -0700

    Fixed bug with changing the color!
    Had to set variable to value.newValue, rather than value

--- canvas-modifier/public/content_script.js
+++ canvas-modifier/public/content_script.js
@@ -6,8 +6,15 @@ chrome.storage.local.get(null, (result) => {applyColorsToPage(result); });
 // 2. Listen for real-time changes made by the user in the React popup
 chrome.storage.onChanged.addListener((changes, areaName) => {
     if (areaName === 'local') 
-    {   
-        applyColorsToPage(changes);
+    {
+        const newColors = {};
+
+        for (const [key, value] of Object.entries(changes))
+        {
+            newColors[key] = value.newValue;
+        }
+
+        applyColorsToPage(newColors);
     }
 });
 
@@ -16,6 +23,7 @@ function applyColorsToPage(colors)
 {
     for (const [key, value] of Object.entries(colors))
     {
+        console.log(value);
         document.documentElement.style.setProperty(key, value);
     }
 }
